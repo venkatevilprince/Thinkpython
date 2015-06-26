0a73fd54a281f8052183ec6345358f6456c2d535
venkatevilprince <venkateshwaracholan@gmail.com>
2015-06-26 11:53:22 +0530

    3.3.py update #2
    prompts user for number of spaces for alignment and lets user to choose whether or not to continue with the program.

--- chapter 3/3.3.py
+++ chapter 3/3.3.py
@@ -2,5 +2,9 @@ def right_justify(word,spaces):
     for i in range(spaces):
         print " ",
     print "%s" %(word)
-
-right_justify("allen",70)    
+decision='y'    
+while(decision=='y' or decision =='Y'):
+    s=raw_input("enter the spaces")
+    s = int(s)
+    right_justify("allen",s)
+    decision=raw_input("Do you want t continue(Press y to continue)")

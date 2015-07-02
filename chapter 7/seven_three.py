0c4b382e3b43232b817e87420afe455a480ed8d1
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:39:42 +0530

    code

--- chapter 7/seven_three.py
+++ chapter 7/seven_three.py
@@ -1,20 +1,14 @@
 from seven_two import *
 import math
+
+
 def test_square_root():
-    
     """Prints the number, its square root in the math module,
        its square root from function in seven_two and the absolute
        difference between both like a table"""
-    
-    for i in range(1,10):
-        
+    for i in range(1, 10):
         print("{0:.1f}".format(i)),
         print("{0:.11f}".format(math.sqrt(i))),
-        print("{0:.11f}".format(square_root(float(i),3))),
-        print("{0:.11f}".format(square_root(float(i),3)-math.sqrt(i)))
-
-
+        print("{0:.11f}".format(square_root(float(i), 3))),
+        print("{0:.11f}".format(square_root(float(i), 3) - math.sqrt(i)))
 test_square_root()
-
-
-        

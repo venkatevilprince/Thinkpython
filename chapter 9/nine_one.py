acafee080a9b1aabca7bfc20f451e33991695b36
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 00:05:09 +0530

    code

--- chapter 9/nine_one.py
+++ chapter 9/nine_one.py
@@ -1,11 +1,7 @@
 """reads all the words from the text file and prints
    the words which has more than 20 characters"""
-
-
 fin = open('words.txt')
 for line in fin:
     word = line.strip()
-    if(len(word)>=20):
+    if(len(word) >= 20):
         print word
-
-    

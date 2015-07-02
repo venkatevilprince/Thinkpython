bd997264f8bd51e25f200d59d54a3e1dd11da311
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:38:23 +0530

    code

--- chapter 7/seven_one.py
+++ chapter 7/seven_one.py
@@ -1,18 +1,11 @@
 import time
+
+
 def countdown(n):
-    
     """counts down by seconds and when it expites it blastsoff
        This is for understanding loops"""
-
     for i in range(n):
-        print n-i
+        print n - i
         time.sleep(1)
     print 'Blastoff!'
-
 countdown(10)
-
-
-
-
-
-        

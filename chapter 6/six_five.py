8f4f636e74267fdf1b5695aa8acb436ba66330f9
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:31:06 +0530

    code

--- chapter 6/six_five.py
+++ chapter 6/six_five.py
@@ -1,17 +1,11 @@
-def ack(m,n):
-    
+def ack(m, n):
     """ this program taught me about maximum
         recursion depth
         if i try larger values it thorws an exception"""
-    
-    if(m==0):
-        return n+1
-    if(n==0 and m>0):
-        return ack(m-1,1)
-    if(n>0 and m>0):
-        return ack(m-1,ack(m,n-1))
-
-
-print ack(3,4)
-
-    
+    if(m == 0):
+        return n + 1
+    if(n == 0 and m > 0):
+        return ack(m - 1, 1)
+    if(n > 0 and m > 0):
+        return ack(m - 1, ack(m, n - 1))
+print ack(3, 4)

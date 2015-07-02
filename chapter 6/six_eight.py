81317fb1bcbd44ae3307e79804225d00485151b9
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:30:10 +0530

    code

--- chapter 6/six_eight.py
+++ chapter 6/six_eight.py
@@ -1,14 +1,9 @@
-def gcd(a,b):
+def gcd(a, b):
     """a simple GCD function for understanding recursion and
        determining the base case for recursion"""
-    
-    if(b==0):
+    if(b == 0):
         return a
     else:
-        r=a%b
-        return gcd(b,r)
-
-
-
-print gcd(60,70)    
-    
+        r = a % b
+        return gcd(b, r)
+print gcd(60, 70)    

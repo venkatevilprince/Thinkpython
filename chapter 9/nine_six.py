5363ba5d62efe2473891c8096e3f4bcde07d64a6
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 00:08:08 +0530

    code

--- chapter 9/nine_six.py
+++ chapter 9/nine_six.py
@@ -1,24 +1,17 @@
 def is_abecederian(word):
-    
     """ Returns True if the letters in a word
         appear in alphabetical order (double letters are ok)
         otherwise Flase"""
-    
-    for a in range(1,len(word)):
-        if( not word[a-1]<=word[a]):
+    for a in range(1, len(word)):
+        if(not word[a - 1] <= word[a]):
             return False
     return True
-
-wordcount=0
-
-
-fin = open('words.txt')        
+wordcount = 0
+fin = open('words.txt')
 for line in fin:
     word = line.strip()
     """how many words in word.txt is abecederian"""
     if is_abecederian(word):
-        wordcount+=1
-
-
-print is_abecederian("aabbccdd") #Test Statement
+        wordcount += 1
+print is_abecederian("aabbccdd")  #Test Statement
 print wordcount

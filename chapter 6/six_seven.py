8ddd34be4e6b6bf0283d8cd053ec799749a97fc0
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:31:47 +0530

    code

--- chapter 6/six_seven.py
+++ chapter 6/six_seven.py
@@ -1,16 +1,10 @@
-def is_power(a,b):
-    
+def is_power(a, b):
     """simple function for understanding recursion
        and base case"""
-    
-    if(a==b):
+    if(a == b):
         return True
-    elif(a%b==0):
-        return is_power(a/b,b)
+    elif(a % b == 0):
+        return is_power(a / b, b)
     else:
         return False
-    
-
-
-
-print is_power(81,9)        
+print is_power(81, 9)

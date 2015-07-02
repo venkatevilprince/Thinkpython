5ce0df7ba291aed688724a3bcb622022bc3b018f
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 22:56:15 +0530

    code

--- chapter 4/four_2.py
+++ chapter 4/four_2.py
@@ -1,29 +1,22 @@
 from four_1 import *
 import math
 from swampy.TurtleWorld import *
-
-
-
-World=TurtleWorld()
-bob=Turtle()
+World = TurtleWorld()
+bob = Turtle()
 print bob
-bob.delay=.001
+bob.delay = .001
 
-def petal(t,r,ang):
+
+def petal(t, r, ang):
     for i in range(2):
-        rarc(t,r,ang)
-        rt(t,180-ang)
+        rarc(t, r, ang)
+        rt(t, 180 - ang)
 
-def flower(t,n,r,ang):
+
+def flower(t, n, r, ang):
     for i in range(n):
-        petal(t,r,ang)
-        lt(t,360.0/n)
-    
-
-
-
-
-
-flower(bob,100,1000,20)
+        petal(t, r, ang)
+        lt(t, 360.0 / n)
+flower(bob, 100, 1000, 20)
 die(bob)
 wait_for_user()

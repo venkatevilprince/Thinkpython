c11c0a1d0c81c894944c1b97d5df7bf513f360f9
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:37:29 +0530

    code

--- chapter 7/seven_four.py
+++ chapter 7/seven_four.py
@@ -1,18 +1,15 @@
 import math
-a=10
+a = 10
+
+
 def eval_loop():
-    
     """Prompts from user and evaluates its expressions
        until the user enter done
        entering  values other than numerical operations would throw an error
        can use initialised variables like a"""
-    
     while(True):
-        decision=raw_input("enter some mathematical operations")
-        if(decision=="done"):
+        decision = raw_input("enter some mathematical operations")
+        if(decision == "done"):
             break
         print eval(decision)
-        
 eval_loop()
-
-

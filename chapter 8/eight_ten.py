28539e71b5fd820dd6e6c1c296ba9136af0aed11
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:49:58 +0530

    code

--- chapter 8/eight_ten.py
+++ chapter 8/eight_ten.py
@@ -1,13 +1,5 @@
 def is_palindrome_modified(word):
-    
     """Returns true if the string is palindrome in a sinle check statement
        without involving loop"""
-    
-    if(word==word[::-1]):
-        return True
-    else :
-        return False
-
-
+        return word==word[::-1]
 print is_palindrome_modified("madam")
-    

c32386e9fffe9e8b7bf1371c06c0edd26a431ac4
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:32:53 +0530

    code

--- chapter 6/six_six.py
+++ chapter 6/six_six.py
@@ -1,18 +1,12 @@
-
-
 def is_palindrome(word):
-    
     """ function checks if the i th element equals the
         (lenth-1-i) elemnet and even if it fails once it
         is not a palindrome """
-    
-    l=len(word)
-    for i in range(l/2+1):
-        if(word[i]!= word[l-1-i]):   
+    l = len(word)
+    for i in range(l / 2 + 1):
+        if(word[i] != word[l - 1 - i]):
             print "not a palindrome"
             return
-    print "palindrome"    
-
-word="redivider"
+    print "palindrome"
+word = "redivider"
 is_palindrome(word)
-

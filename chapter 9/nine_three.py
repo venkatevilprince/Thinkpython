8d7dbcac3538c1643bae587a18a87fd84fe30584
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 00:09:37 +0530

    code

--- chapter 9/nine_three.py
+++ chapter 9/nine_three.py
@@ -1,24 +1,19 @@
-def avoids(word,forbidden_letters):
-    
+def avoids(word, forbidden_letters):
     """Returns True if the word does not contain any
        letter from the forbidden word"""
-
     for letter in word:
         for fl in forbidden_letters:
-            if(letter==fl):
+            if(letter == fl):
                 return False
     return True
-            
-
-totalwords=0    
-wordcount=0
-user_input=raw_input("Enter the forbidden Letters as a word\"(example aeiou)\"")
-fin = open('words.txt')        
+totalwords = 0
+wordcount = 0
+user_input = raw_input("Enter the forbidden Letters as a word\"(example aeiou)\"")
+fin = open('words.txt')
 for line in fin:
     word = line.strip()
-    totalwords+=1
-    if(avoids(word,user_input)):
-        wordcount+=1
-
-print "{} words in the file words.txt does not have the letter {}".format(wordcount,user_input) 
-#print totalwords 
+    totalwords += 1
+    if(avoids(word, user_input)):
+        wordcount += 1
+print "{} words in the file words.txt does not have the letter {}".format(wordcount, user_input)
+#print totalwords

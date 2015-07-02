dc95f4e49736998affd245bd4405022f767bcd61
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 22:43:52 +0530

    code

--- chapter 3/3.4.py
+++ chapter 3/3.4.py
@@ -1,13 +1,16 @@
-def do_twice(f,g):
+def do_twice(f, g):
     f(g)
     f(g)
+
+
 def print_twice(word):
     print word
-print "prints twice"    
-do_twice(print_twice,"spam")
+print "prints twice"
+do_twice(print_twice, "spam")
 
-def do_four(x,y):
-    do_twice(x,y)
-    do_twice(x,y)
+
+def do_four(x, y):
+    do_twice(x, y)
+    do_twice(x, y)
 print "prints four times"
-do_four(print_twice,"spam")    
+do_four(print_twice, "spam")

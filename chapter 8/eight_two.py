14544f2fa2adcf8d3e24e46e9d3f5b3f1ec44b13
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:57:03 +0530

    code

--- chapter 8/eight_two.py
+++ chapter 8/eight_two.py
@@ -1,20 +1,10 @@
 prefixes = 'JKLMNOPQ'
 suffix = 'ack'
-
 for letter in prefixes:
-    
     """changes suffix to uack for O and Q so that it spells correct
        Ouack and Quack"""
-    
     if(letter == 'O' or letter ==  'Q'):
-        suffix="uack"
+        suffix = "uack"
     else:
-        suffix="ack"    
+        suffix = "ack"    
     print letter + suffix
-
-
-
-
-
-
-    

9a5d492f562ebc4e18b4c3f679749403e3945f03
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 22:45:26 +0530

    code

--- chapter 3/3.5.py
+++ chapter 3/3.5.py
@@ -1,29 +1,23 @@
-def draw_pattern1(n,m):
+def draw_pattern1(n, m):
     for j in range(m):
             print "+",
             for k in range(4):
                 print "-",
     print"+"
-def draw_grid(n,m):
+
+
+def draw_grid(n, m):
     for i in range(n):
-        draw_pattern1(n,m)
+        draw_pattern1(n, m)
         for l in range(4):
-            for l2 in range(m+1):
+            for l2 in range(m + 1):
                 print "|        ",
-            print""    
-    draw_pattern1(n,m)
+            print""
+    draw_pattern1(n, m)
 # parameters are rows and columns
 # u can try different rows and columns
-
-r=raw_input("enter the rows")
-r=int(r)
-c=raw_input("enter the  columns")
-c=int(c)
-draw_grid(r,c)
-
-
-
-
-
-
-    
+r = raw_input("enter the rows")
+r = int(r)
+c = raw_input("enter the  columns")
+c = int(c)
+draw_grid(r, c)

86545bfc7003e6797863f94132d9da41744aef43
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 00:00:42 +0530

    code

--- chapter 9/nine_five.py
+++ chapter 9/nine_five.py
@@ -1,32 +1,24 @@
 from nine_four import *
 
 
-
-def uses_all(word,letters):
-    
+def uses_all(word, letters):
     """Returns True if the word uses all the characters in letters
        atleast once"""
-    
-    if(uses_only(word,letters)):
-        if(uses_only(letters,word)):
+    if(uses_only(word, letters)):
+        if(uses_only(letters, word)):
             return True
-        else :
+        else:
             return False
     return False
-
 wordcount = 0
-
-fin = open('words.txt')        
+fin = open('words.txt')
 for line in fin:
     word = line.strip()
-    
     """how many words uses all the letters "aeiou
 
        Change the "aeiou" in the function parameter
        to check for other values"""
-    
-    if uses_all(word,"aeiou"):
-        wordcount+=1
-
-print uses_all("pills","pils") #test statement
+    if uses_all(word, "aeiou"):
+        wordcount += 1
+print uses_all("pills", "pils")  #test statement
 print wordcount

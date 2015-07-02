1c1420b7cfd97de36ecc79ae4cb362cf34e6d41f
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 22:30:36 +0530

    code

--- chapter 12/twelve_one.py
+++ chapter 12/twelve_one.py
@@ -1,14 +1,7 @@
-
 def sumall(*a):
-
     """ Returns the sum of any number of arguments passed"""
-    
-    sum=0
+    sum = 0
     for i in range(len(a)):
-        sum=sum+a[i]
-
+        sum = sum + a[i]
     return sum
-
-
-
-print sumall(1,1,1,1,1,1)
+print sumall(1, 1, 1, 1, 1, 1)

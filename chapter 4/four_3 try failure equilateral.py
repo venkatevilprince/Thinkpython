7fad849f6715adbcd5b38f5d7902942e7d3fe8a5
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 22:57:25 +0530

    code

--- chapter 4/four_3 try failure equilateral.py
+++ chapter 4/four_3 try failure equilateral.py
@@ -1,31 +1,22 @@
 from four_1 import *
 import math
 from swampy.TurtleWorld import *
-
-World=TurtleWorld()
-bob=Turtle()
+World = TurtleWorld()
+bob = Turtle()
 print bob
-bob.delay=.001
+bob.delay = .001
 
 
-
-def draw_triangle(t,len):
+def draw_triangle(t, len):
     for i in range(3):
-        fd(t,len)
-        lt(t,180-60)
-
+        fd(t, len)
+        lt(t, 180 - 60)
 #draw_triangle(bob,100)
 
-def draw_shape(t,len,n):
+
+def draw_shape(t, len, n):
     for i in range(n):
-        draw_triangle(t,len)
-        lt(t,360.0/n)
-
-draw_shape(bob,100,5)
-
-
+        draw_triangle(t, len)
+        lt(t, 360.0 / n)
+draw_shape(bob, 100, 5)
 wait_for_user()
-
-
-
-    

03a943628a5ce668063b5775dd5e56b686466ed7
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 00:02:04 +0530

    code

--- chapter 9/nine_four.py
+++ chapter 9/nine_four.py
@@ -1,29 +1,22 @@
-def check(singlechar,word):
-    
+def check(singlechar, word):
     """Return True if the single character is present
        in the word otherwise false"""
-
-    flag=0
+    flag = 0
     for letter in word:
-        if(singlechar==letter):
-            flag=1    
-    if(flag==1):
+        if(singlechar == letter):
+            flag = 1
+    if(flag == 1):
         return True
     else:
         return False
 
 
-
-def uses_only(word,letters):
-
+def uses_only(word, letters):
     """Returns True if the word is formed only with the letters
        otherwise Flase"""
-
     for i in word:
-        if(not check(i,letters)):
+        if(not check(i, letters)):
             return False
-        
     return True
-                
 if __name__ == "__main__":
-    print uses_only("pills","pils")
+    print uses_only("pills", "pils")

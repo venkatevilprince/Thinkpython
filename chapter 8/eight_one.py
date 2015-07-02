45a81e1858c5ae4d950fdef17440c2206f36f76c
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:44:55 +0530

    code

--- chapter 8/eight_one.py
+++ chapter 8/eight_one.py
@@ -1,10 +1,5 @@
 def reverse_print(word):
-    
     """prints a word in reverse one per line"""
-    
     for i in range(len(word)):
-        print word[len(word)-1-i]
-
-
-reverse_print("hello")        
-        
+        print word[len(word) - 1 - i]
+reverse_print("hello")

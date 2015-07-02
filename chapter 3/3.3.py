110c34c42205fef5cd1c574a0547322dab0a3c9d
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 22:42:39 +0530

    code

--- chapter 3/3.3.py
+++ chapter 3/3.3.py
@@ -1,10 +1,10 @@
-def right_justify(word,spaces):
+def right_justify(word, spaces):
     for i in range(spaces):
         print " ",
-    print "%s" %(word)
-decision='y'    
-while(decision=='y' or decision =='Y'):
-    s=raw_input("enter the spaces")
+    print "%s" % (word)
+decision = 'y'
+while(decision == 'y' or decision == 'Y'):
+    s = raw_input("enter the spaces")
     s = int(s)
-    right_justify("allen",s)
-    decision=raw_input("Do you want t continue(Press y to continue)")
+    right_justify("allen", s)
+    decision = raw_input("Do you want t continue(Press y to continue)")

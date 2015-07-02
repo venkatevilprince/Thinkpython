a50595b25c0604f5f3de600f7bc8cfd1c274ef88
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 22:37:37 +0530

    code

--- chapter 12/twelve_two.py
+++ chapter 12/twelve_two.py
@@ -1,9 +1,8 @@
 import random
 
-def sort_by_length(words):
 
+def sort_by_length(words):
     """ sorts with 2 argument tuple"""
-    
     t = []
     for word in words:
         t.append((len(word), word))
@@ -11,31 +10,22 @@ def sort_by_length(words):
     print t
     res = []
     for length, word in t:
-        print length,word
+        print length, word
         res.append(word)
     return res
 
 
 def sort_by_length_random(words):
-
     """ sorts with 3 argument tuple (length,random priority,word)"""
-    
     t = []
     for word in words:
-       t.append((len(word), random.random(), word))
-
+        t.append((len(word), random.random(), word))
     t.sort(reverse=True)
     #print t
     res = []
     for length, _, word in t:
-        print length,_,word
+        print length, _, word
         res.append(word)
     return res
-
-test=['a','b','c','ab','bc','aav','accc','ddda']
-
+test = ['a', 'b', 'c', 'ab', 'bc', 'aav', 'accc', 'ddda']
 print sort_by_length_random(test)
-
-
-
-

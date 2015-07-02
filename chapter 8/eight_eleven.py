5951c420a5240737e7c6253a0233cd915c50c6eb
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-02 23:41:39 +0530

    code

--- chapter 8/eight_eleven.py
+++ chapter 8/eight_eleven.py
@@ -8,6 +8,8 @@ def any_lowercase1(s):
             return True
         else:
             return False
+
+
 def any_lowercase2(s):
     """ This will always be true"""
     for c in s:
@@ -15,22 +17,26 @@ def any_lowercase2(s):
             return 'True'
         else:
             return 'False'
+
+
 def any_lowercase3(s):
     """ Checks whether the first letter is lower """
     for c in s:
         flag = c.islower()
         return flag
+
+
 def any_lowercase4(s):
     """ Checks whether the first letter is lower """
     flag = False
     for c in s:
         flag = flag or c.islower()
     return flag
+
+
 def any_lowercase5(s):
     """returns None if everything is lower
        false if even a singe character is not lower"""
     for c in s:
         if not c.islower():
             return False
-
-

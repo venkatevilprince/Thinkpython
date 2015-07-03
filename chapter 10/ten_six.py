cec2f963c64ca1a4f4df2bd8002ff954d8ee3618
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 08:51:29 +0530

    code

--- chapter 10/ten_six.py
+++ chapter 10/ten_six.py
@@ -1,14 +1,9 @@
 def is_sorted(item):
-
-    for i in range(len(item)-1):
-        if not item[i]<item[i+1]:
+    for i in range(len(item) - 1):
+        if not item[i] < item[i + 1]:
             return False
-
     return True
-
-
-
-test=[1,2,3,4,5,6,11]
-test2=['b','a']
+test = [1, 2, 3, 4, 5, 6, 11]
+test2 = ['b', 'a']
 print is_sorted(test)
 print is_sorted(test2)

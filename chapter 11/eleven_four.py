e14aab71dda4e141e6fb0d53f8f21e38010d4950
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:15:42 +0530

    code

--- chapter 11/eleven_four.py
+++ chapter 11/eleven_four.py
@@ -1,21 +1,15 @@
 from eleven_two import *
 
-def reverse_lookup(a,n):
 
+def reverse_lookup(a, n):
     """ Returns a list of keys for a given dictionary value """
-    
-    l=[]
-    for key,val in a.items():
-
-        if(val==n):
+    l = []
+    for key, val in a.items():
+        if(val == n):
             #print val," ",key
             l.append(key)
-    
     return l
+s = "apple"
+a = histogram(s)
 
-
-
-s="apple"
-a=histogram(s)
-
-print 1," : ",reverse_lookup(a,1)
+print 1, " : ", reverse_lookup(a, 1)

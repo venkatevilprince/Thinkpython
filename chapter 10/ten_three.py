25321e8100a46282050c931dc1ce46d464ed124e
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 08:57:42 +0530

    code

--- chapter 10/ten_three.py
+++ chapter 10/ten_three.py
@@ -1,22 +1,15 @@
 def cumulative_sum(item):
-
     """Return the cumulative sum of the list passed"""
-    
-    a=[];
+    a = [];
     for i in range(len(item)):
-        sum=0
-        for j in range(i+1):
+        sum = 0
+        for j in range(i + 1):
             #print "a",
-            sum=sum+item[j]
+            sum = sum + item[j]
             #print sum
         print""
         #print sum
         a.append(sum)
-            
-        
     return a
-
-
-test=[1,2,3,4,5]
-
+test=[1, 2, 3, 4, 5]
 print cumulative_sum(test)        

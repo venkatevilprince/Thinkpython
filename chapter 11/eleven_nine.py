05a17d7c1a9dd14d4412430e0361a73fdbe98c29
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:16:15 +0530

    code

--- chapter 11/eleven_nine.py
+++ chapter 11/eleven_nine.py
@@ -1,20 +1,12 @@
 def has_duplicates(item):
-
     """Returns true if items has duplicate values"""
-    
     for i in item:
         if i in box.keys():
             return True
         else:
-            box[i]=1
+            box[i] = 1
     return False        
-
-
-box={}
-
-
-test=[1,2,3,4,5,7,6,3,3,4,5]
-
-
+box = {}
+test=[1, 2, 3, 4, 5, 7, 6, 3, 3, 4, 5]
 print has_duplicates(test)
 print box

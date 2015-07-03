534bdc8d88a07293f8199393a89cc073408ef085
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 08:50:41 +0530

    code

--- chapter 10/ten_seven.py
+++ chapter 10/ten_seven.py
@@ -1,23 +1,15 @@
-def is_anagram(word1,word2):
-
+def is_anagram(word1, word2):
     """Returns True of the words are anagrams"""
-    
-    if len(word1)!=len(word2):
+    if len(word1) != len(word2):
         return False
-
     a=''.join(sorted(word1))
     #print a
     b=''.join(sorted(word2))
     #print b
-              
     if  a == b:
         return True
     else:
         return False
-
-
-
-test1="abngela"
-test2="gleanab"
-
-print is_anagram(test1,test2)
+test1 = "abngela"
+test2 = "gleanab"
+print is_anagram(test1, test2)

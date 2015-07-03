b4982cda7679db65cdd872315c742d70527862f8
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 08:46:42 +0530

    code

--- chapter 10/ten_four.py
+++ chapter 10/ten_four.py
@@ -1,14 +1,7 @@
 def middle(item):
-    a=[]
-    for i in range(1,len(item)-1):
+    a = []
+    for i in range(1, len(item) - 1):
         a.append(item[i])
-
     return a    
-
-
-
-test=[1,2,3,4,5,6]
-
+test=[1, 2, 3, 4, 5, 6]
 print middle(test)
-        
-    

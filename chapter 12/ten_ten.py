fa18b0a48d55747b67b46cba82a909ef1ecbd3ae
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:26:26 +0530

    code

--- chapter 12/ten_ten.py
+++ chapter 12/ten_ten.py
@@ -1,51 +1,37 @@
 import time
 
 
-def read_words(s='words.txt'):
-
+def read_words():
     """executes faster"""
-    a=[]
-    with open(s, 'r') as f:
-        #read_data = f.read()
-        #print read_data
-        for b in f:
-            #print b
-            for w in b.split():
-                a.append(w)
-
-    return a
-
-def read_words2():
-
-    """takes almost 30 seconds to execute"""
-    
-    a=[]
+    a = []
     with open('words.txt', 'r') as f:
         #read_data = f.read()
         #print read_data
         for b in f:
             #print b
             for w in b.split():
-                a=a+[w]
-
+                a.append(w)
     return a
 
-if __name__=="__main__":
+
+def read_words2():
+    """takes almost 30 seconds to execute"""
+    a = []
+    with open('words.txt', 'r') as f:
+        #read_data = f.read()
+        #print read_data
+        for b in f:
+            #print b
+            for w in b.split():
+                a = a + [w]
+    return a
+if __name__ == "__main__":
     start = time.time()
-    n=read_words()
+    n = read_words()
     end = time.time()
-
-
     print end - start
     start = time.time()
-    n=read_words2()
+    n = read_words2()
     end = time.time()
-
     print end - start
     print"done"
-
-
-
-
-
-        

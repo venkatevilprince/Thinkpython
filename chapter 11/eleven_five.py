772e2a8ae13a1105da73947afa7a58f352d6e46a
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:14:59 +0530

    code

--- chapter 11/eleven_five.py
+++ chapter 11/eleven_five.py
@@ -1,40 +1,26 @@
 from eleven_two import *
 
-def invert_dict(a):
-    inv={}
 
+def invert_dict(a):
+    inv = {}
     """ Returns a reversed dictionary"""
-    
-    for key,val in a.items():
-        print val," ",key
+    for key, val in a.items():
+        print val, " ", key
         if val not in inv.keys():
-            inv[val]=[key]
+            inv[val] = [key]
         else:
             inv[val].append(key)
-        
-
-    return inv    
-
-    
+    return inv
 
 
 def invert_dict2(a):
-
     """Returns a reversed dictionary with set default"""
-    inv={}
-
-    for key,val in a.items():
-        inv.setdefault(val,[])
+    inv = {}
+    for key, val in a.items():
+        inv.setdefault(val, [])
         inv[val].append(key)
-        
-
-    return inv 
-
-
-
-s="apple"
-a=histogram(s)
+    return inv
+s = "apple"
+a = histogram(s)
 print a
 print invert_dict2(a)
-    
-

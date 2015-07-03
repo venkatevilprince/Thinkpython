3bd32fb4e8b1e1d0a453dab33eb53e30fac9eed1
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 08:59:39 +0530

    code

--- chapter 10/ten_two.py
+++ chapter 10/ten_two.py
@@ -1,21 +1,14 @@
 def capitalise_nested(list1):
-
     """Returns the capitalised nested list"""
-
-    
-    a=[]
+    a = []
     for i in range(len(list1)):        
-        if isinstance(list1[i],list):
+        if isinstance(list1[i], list):
             #print"1"
             a.append(capitalise_nested(list1[i]))
         else:
             #print "2"
             a.append(list1[i].capitalize())
     return a
-
-
-test_list=['a','b',['c',['d','e','f'],'g'],'h','i']
-
+test_list=['a', 'b' ,['c' ,['d' ,'e' ,'f'], 'g'], 'h', 'i']
 print capitalise_nested(test_list)
-
 #print test_list[0].capitalize()

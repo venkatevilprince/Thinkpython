294f7f669ced7e5eafe2d5b6993db93201e69532
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 17:23:47 +0530

    code

--- chapter 13/thirteen_three_frequent_word.py
+++ chapter 13/thirteen_three_frequent_word.py
@@ -2,20 +2,20 @@ from thirteen_two_unique_words_book import *
 
 
 def most_frequent(box,n):
-    res=[]
-    for x,y in box.items():
+    res = []
+    for x, y in box.items():
         res.append((y,x))
     res.sort(reverse=True)
-    for x,y in res[0:n]:
-        print x,y
+    for x, y in res[0:n]:
+        print x, y
     return res
 if __name__ == "__main__":
     book1 = "Catching Fire"
-    bookhist1 = read_and_count(book1+".txt")
+    bookhist1 = read_and_count(book1 + ".txt")
     book2 = "The Hunger Games"
-    bookhist2 = read_and_count(book2+".txt")
+    bookhist2 = read_and_count(book2 + ".txt")
     print book1
-    booklist1=most_frequent(bookhist1,20)
+    booklist1 = most_frequent(bookhist1, 20)
     print book2
-    booklist2=most_frequent(bookhist2,20)
+    booklist2 = most_frequent(bookhist2,2 0)
     

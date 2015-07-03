b2a43823ff8e0bfbf065a94f7a2eb8417fb3e027
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:20:02 +0530

    code

--- chapter 11/eleven_six.py
+++ chapter 11/eleven_six.py
@@ -1,31 +1,27 @@
-known = {0:0, 1:1}
 import time
-def fibonacci(n):
+known = {0: 0, 1: 1}
 
+
+def fibonacci(n):
     """memorised version of  fibonacci"""
-    
     if n in known:
         return known[n]
-    res = fibonacci(n-1) + fibonacci(n-2)
+    res = fibonacci(n - 1) + fibonacci(n - 2)
     known[n] = res
     return res
-
-start=time.time()
+start = time.time()
 print fibonacci(10)
-end=time.time()
-print end-start
+end = time.time()
+print end - start
 print known
 
 
 def fibonacci_rec(n):
-
     """ Recursive version of fibonacci"""
-
-    if(n==0):
+    if(n == 0):
         return 0
-    return n+fibonacci_rec(n-1)
-
-start=time.time()
+    return n + fibonacci_rec(n - 1)
+start = time.time()
 print fibonacci_rec(10)
-end=time.time()
-print end-start
+end = time.time()
+print end - start

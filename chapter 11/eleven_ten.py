99b10124029f64a3a681a3e33e343c182ded4ee6
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:21:46 +0530

    code

--- chapter 11/eleven_ten.py
+++ chapter 11/eleven_ten.py
@@ -1,31 +1,24 @@
 from ten_ten import *
 from eight_twelve import*
 from ten_eleven import *
-data=read_words()
-box={}
+data = read_words()
+box = {}
+
 
 def is_rotate(data):
-
     """ Prints valid words from all possible rotations of each word
         in words.txt and stores them in a dictionary """
-    
-    s1=10;s2=10
+    s1 = 10; s2 = 10
     for i in range(len(data)):
-
-        for j in range(1,26):
-
-            t=bisect(data,rotate_word(data[i],j))
+        for j in range(1, 26):
+            t = bisect(data, rotate_word(data[i], j))
             if(t):
-                box[data[i]]=t
+                box[data[i]] = t
                 print data[i],
-                for x in range(10-len(data[i])):
+                for x in range(10 - len(data[i])):
                     print "",
-                print j,    
-                for y in range(10-len(str(j))):
+                print j,
+                for y in range(10 - len(str(j))):
                     print "",
                 print t
-
-
-
 is_rotate(data)
-

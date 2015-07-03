a916f1f90629c9053c91b3868ef0d30135bf7bd1
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 08:49:53 +0530

    code

--- chapter 10/ten_one.py
+++ chapter 10/ten_one.py
@@ -1,20 +1,12 @@
 def nested_sum(list1):
-
     """Returns the sum of nested lists"""
-
-    
-    nsum=0
+    nsum = 0
     for i in range(len(list1)):        
-        if isinstance(list1[i],list):
-            nsum=nsum+nested_sum(list1[i])
+        if isinstance(list1[i], list):
+            nsum = nsum+nested_sum(list1[i])
         else:
-            nsum=nsum+list1[i]
+            nsum = nsum + list1[i]
     return nsum
-
-
-test_list=[1,1,[1,[1,1,1],1],1,1]
-
+test_list=[1, 1, [1, [1, 1, 1], 1], 1, 1]
 #print nested_sum(test_list);
-
-
 print nested_sum(test_list)

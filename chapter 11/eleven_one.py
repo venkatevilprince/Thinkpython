5ee32567d6b71a82b24cb471196bd490329cdf01
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:17:09 +0530

    code

--- chapter 11/eleven_one.py
+++ chapter 11/eleven_one.py
@@ -1,33 +1,16 @@
 from ten_ten import *
 import time
-
 """ Prints Time taken by the in statement to find a value"""
-
-words={}
-data=read_words()
-
+words = {}
+data = read_words()
 for i in range(len(data)):
-    words[i]=data[i]
-
-find="catapult"
-
-start=time.time()
-
+    words[i] = data[i]
+find = "catapult"
+start = time.time()
 #print words 
-
 if "apple" in words.values():
     #print key,val
     #if(val==find):
     print"true"
-    
-    
-        
-        
-
-end=time.time()
-
-print end-start
-
-
-
-
+end = time.time()
+print end - start

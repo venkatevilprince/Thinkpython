b3ed7c4c5070d9864a441101f5d89cf620c96dc2
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 08:58:53 +0530

    code

--- chapter 10/ten_twelve.py
+++ chapter 10/ten_twelve.py
@@ -1,20 +1,14 @@
 from ten_eleven import *
+data = read_words()
 
-data=read_words()
 
 def find_reverse(data):
-
     """prints words that can be formed by reversing a word"""
-    
     for a in data:
-        
-        t= bisect(data,a[::-1])
+        t = bisect(data, a[::-1])
         if(t):
             print a,
-            for i in range(10-len(a)):
+            for i in range(10 - len(a)):
                 print "",
             print t
-            
-
-
 find_reverse(data)

6f44c57aaaf4d93c0aeda6e5b4db7e1f756ad588
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:18:06 +0530

    code

--- chapter 11/eleven_seven.py
+++ chapter 11/eleven_seven.py
@@ -1,25 +1,19 @@
-mem={}
-
+mem = {}
 import time
 
-def ack(m,n):
-    
+
+def ack(m, n):
     """ this program taught me about try and exception
-
         Prints all the memorised vaues"""
-    
-    if(m==0):
-        return n+1
-    if(n==0 and m>0):
-        return ack(m-1,1)
-    
-
+    if(m == 0):
+        return n + 1
+    if(n == 0 and m > 0):
+        return ack(m - 1, 1)
     try:
-        return mem[m,n]
+        return mem[m, n]
     except KeyError:
-        mem[m,n]=ack( m-1,ack(m,n-1))
-        return mem[m,n]
-
-print ack(3,4)
-print ack(1,344)
+        mem[m, n] = ack( m - 1, ack(m, n - 1))
+        return mem[m, n]
+print ack(3, 4)
+print ack(1, 344)
 print mem 

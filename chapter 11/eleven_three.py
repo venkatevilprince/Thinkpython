54c8ae7b2d78066b177983173942831e71565bd8
venkateshwaracholan <venkateshwaracholan@gmail.com>
2015-07-03 10:22:33 +0530

    code

--- chapter 11/eleven_three.py
+++ chapter 11/eleven_three.py
@@ -1,18 +1,9 @@
 from eleven_two import *
 
+
 def print_hist(a):
-
     for i in sorted(a):
-        print i," ",a[i]
-    
-
-
-s="apple"
-a=histogram(s)
-
+        print i, " ", a[i]
+s = "apple"
+a = histogram(s)
 print_hist(a)
-
-
-
-
-
